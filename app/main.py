3a3ff06057b4af7d3efb2e7bedd981a6df4d0e05
paxtonBravo <96046448+paxtonBravo@users.noreply.github.com>
2024-05-08 17:14:14 +0800

    fisrt push

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Query
+from stock.real_time import stock, index
+from typing import List
+
+app = FastAPI()
+
+
+@app.get("/stock")
+def get_stock_data(code: str = Query(str)):
+    return stock(code)
+
+
+@app.get("/stock/multi")
+def get_multi_stock_data(codes: List[str] = Query([])):
+    return stock(codes)
+
+
+@app.get("/index")
+def get_index_data():
+    return index()
---
+++ test.py
@@ -0,0 +1,3 @@
+from stock.real_time import stock
+
+print(stock(2330))
