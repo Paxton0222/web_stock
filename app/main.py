069fdbc8d5775769004793570846158c20f8163f
Paxton <paxton02.co@gmail.com>
2024-11-07 14:01:02 +0800

    feat: 加入更新股票資料表 API

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Query
 from stock.real_time import stock, index
 from typing import List
+import twstock
 
 app = FastAPI()
 
@@ -14,6 +15,13 @@ def get_stock_data(code: str = Query(str)):
 def get_multi_stock_data(code: List[str] = Query([])):
     return stock(code)
 
+
+@app.get('/stock/update')
+def get_update():
+    twstock.__update_codes()
+    return {
+        "status": True
+    }
 
 @app.get("/index")
 def get_index_data():
